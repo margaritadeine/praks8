edc020dcd6578dbb41533459e4d239f450bebab5
Margarita Deinekina <margarita.deinekina@khk.ee>
2018-10-10 16:31:33 +0300

    Inkrement inputi loomine.

--- main.js
+++ main.js
@@ -1,17 +1,33 @@
 let numInput = document.querySelector('#numInput');
-let factOutput = document.querySelector('#factOutput')
+let factOutput = document.querySelector('#factOutput');
+let incInput = document.querySelector('#incInput');
 
 const getFact = () => {
-    let number = numInput.value;
+    factOutput.innerHTML = ''
     
-    if(number != ''){
-        axios.get('http://numbersapi.com/'+number)
+    let number = numInput.value;
+    let inc = incInput.value;
+    let baseUrl = 'http://numbersapi.com/'
+    //vaja saada sarnane URL
+    //http://numbersapi.com/1..3
+    let url =`${baseUrl}${number}..${parseInt(number) + parseInt(inc)}`;
+    
+    
+    if(number != '' && inc != ''){
+        axios.get(url)
         .then(response => {
-            factOutput.innerText = response.data;
+            for (let item in response.data){
+                let li = document.createElement('li');
+                li.innerText = response.data[item];
+                factOutput.appendChild(li);
+            }
+           
         })
         .catch(error => {
             console.log(error);
         })
-    }   
+    }  
 }
-numInput.addEventListener('input', getFact)
+
+numInput.addEventListener('input', getFact);
+incInput.addEventListener('input', getFact);
