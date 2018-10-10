f040e3ac5df03dc5de0fe0906a187e2c22256851
Margarita Deinekina <margarita.deinekina@khk.ee>
2018-10-10 15:40:46 +0300

    HTML ja js failid lehe "Numbri faktid" jaoks.

---
+++ main.js
@@ -0,0 +1,17 @@
+let numInput = document.querySelector('#numInput');
+let factOutput = document.querySelector('#factOutput')
+
+const getFact = () => {
+    let number = numInput.value;
+    
+    if(number != ''){
+        axios.get('http://numbersapi.com/'+number)
+        .then(response => {
+            factOutput.innerText = response.data;
+        })
+        .catch(error => {
+            console.log(error);
+        })
+    }   
+}
+numInput.addEventListener('input', getFact)
